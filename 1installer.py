29cbbc831453de15143bc2c07adc2b58e1ec6803
yechan hong <yechanhong@gmail.com>
2019-09-17 08:38:06 -0700

    fixed name capitalization issues

--- 1installer.py
+++ 1installer.py
@@ -16,8 +16,8 @@ if OS == 'MAC':
 elif OS == 'WIN':
   rsc_game = os.path.join(common_dir,'HyperLightDrifter')
   replace_dir = join(rsc_installer,'WIN_replace')
-  replace_pairs = [('rm_c_backertabletx.lvl','central','Central')]
-  replace_pairs += [('rm_in_01_brokenshallows.lvl','intro','Intro')]
+  replace_pairs = [('rm_c_backertabletx.lvl','rm_C_BackerTabletX.lvl','central','Central')]
+  replace_pairs += [('rm_in_01_brokenshallows.lvl','rm_IN_01_brokenshallows.lvl','intro','Intro')]
   copy_pairs = [('rm_wc_minilab_layer0_sketch.png','west','West')]
   copy_pairs += [('rm_wc_minilab_layer1_sketch.png','west','West')]
   copy_pairs += [('rm_wc_minilab_layer1_sketch_baked.png','west','West')]
@@ -25,9 +25,9 @@ elif OS == 'WIN':
   copy_pairs += [('rm_wc_minilab_layer2_sketch.png','west','West')]
   copy_pairs += [('phrases.txt', '', '')]
 
-for filename, rsc_dir_src, rsc_dir_dest in replace_pairs:
+for filename, filename_dest, rsc_dir_src, rsc_dir_dest in replace_pairs:
   file_src = join(rsc_installer,rsc_dir_src, filename)
-  file_dest = join(rsc_game,rsc_dir_dest, filename)
+  file_dest = join(rsc_game,rsc_dir_dest, filename_dest)
   util.replace_file(file_src, file_dest)
     
 for filename, rsc_dir_src, rsc_dir_dest in copy_pairs:
